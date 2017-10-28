afec3170a3b8d3b21039384d3d5619ded2d8c466
Aaron Osher <aaron@oshers.com>
2017-10-28 16:18:06 +0100

    Finisher image routes

--- server/post_to_twitter/src/server.js
+++ server/post_to_twitter/src/server.js
@@ -1,8 +1,14 @@
 import express from "express"
 import environment from "node-env-file"
 
+// Route imports
+import image from "./routes/image/image"
+
 let env = environment(".env")
 let app = express()
+
+// Routes
+image(app)
 
 app.listen(env.PORT, env.HOST)
 console.log("Server running on " + env.HOST + ":" + env.PORT + ". Use ctrl+c to stop.")
