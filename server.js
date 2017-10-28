3db882655e22cb471135048da01e27f4e77f772e
Ollie Favell <Zattri@users.noreply.github.com>
2017-10-28 22:03:11 +0100

    Ollie (#16)
    * Added in functionality to itterate through array of images
    
    * Separated body parts and got base template
    
    * Added randomise button functionality and added comments to code
    
    * Hotfix to get master working again
    
    * First working octocat - pusheen
    
    * Added new cat head pictures
    
    * 2nd octocat in"
    
    * Added new styles ready to make buttons nicer
    
    * Added Node Js server
    
    * Made it serveable of HTTP (#15)
    
    * Fixed button formatting

--- server.js
+++ server.js
@@ -4,10 +4,37 @@ let fs = require('fs')
 
 let app = express()
 
+// Get all files in a folder and return an array containing their names
+function getFiles(dir) {
+    fileList = [];
+    var files = fs.readdirSync(dir);
+    for (var i in files) {
+        if (!files.hasOwnProperty(i)) continue;
+        var name = dir + '/' + files[i];
+        if (!fs.statSync(name).isDirectory()) {
+            fileList.push(name);
+        }
+    }
+    return fileList;
+}
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get("/files", function(req, res) {
+    // Sub image arrays
+    var headImgArray = getFiles('images/head/')
+    var armImgArray = getFiles('images/arms/')
+    var legImgArray = getFiles('images/legs/')
+
+    // Main array of image arrays
+    var mainArray = [headImgArray, armImgArray, legImgArray]
+
+    res.json(mainArray)
 })
 
 app.use(express.static('.'))
 
 app.listen("8080", "127.0.0.1")
+console.log("Server running on 127.0.0.1:8080. Use ctr+c to exit.")
