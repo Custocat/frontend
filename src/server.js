07a68f96df6368c5de128e885ecf8f7ce2d7abe0
Aaron Osher <aaron@oshers.com>
2017-11-07 03:05:56 +0000

    Update server.js meets eslint style requirements

--- src/server.js
+++ src/server.js
@@ -1,33 +1,34 @@
-let express = require('express')
-let path = require('path')
-let fs = require('fs')
-let env = require('node-env-file')('.env')
+import express from "express"
+import path from "path"
+import fs from "fs"
+import environment from "node-env-file"
+let env = environment(".env")
 
 let app = express()
 
 // Get all files in a folder and return an array containing their names
 function getFiles(dir) {
-    let fileList = [];
-    var files = fs.readdirSync(dir);
+    let fileList = []
+    var files = fs.readdirSync(dir)
     for (var i in files) {
-        if (!files.hasOwnProperty(i)) continue;
-        var name = dir + '/' + files[i];
+        if (!files.hasOwnProperty(i)) continue
+        var name = dir + "/" + files[i]
         if (!fs.statSync(name).isDirectory()) {
-            fileList.push(name.replace(__dirname + '/public/', ''));
+            fileList.push(name.replace(__dirname + "/public/", ""))
         }
     }
-    return fileList;
+    return fileList
 }
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'))
+    res.sendFile(path.join(__dirname + "/public/index.html"))
 })
 
 app.get("/files", function(req, res) {
     // Sub image arrays
-    var headImgArray = getFiles(__dirname + '/public/images/head/')
-    var armImgArray = getFiles(__dirname + '/public/images/arms/')
-    var legImgArray = getFiles(__dirname + '/public/images/legs/')
+    var headImgArray = getFiles(__dirname + "/public/images/head/")
+    var armImgArray = getFiles(__dirname + "/public/images/arms/")
+    var legImgArray = getFiles(__dirname + "/public/images/legs/")
 
     // Main array of image arrays
     var mainArray = [headImgArray, armImgArray, legImgArray]
@@ -35,11 +36,11 @@ app.get("/files", function(req, res) {
     res.json(mainArray)
 })
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"))
 
 app.listen(env.PORT, env.HOST)
 console.log("Server running on " + env.HOST + ":" + env.PORT + ". Use ctr+c to exit.")
 
-if (process.argv.slice(2)[0] == 'test') {
+if (process.argv.slice(2)[0] == "test") {
     process.exit(0)
 }
